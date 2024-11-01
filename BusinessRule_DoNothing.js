b64f629849c3337ec2d19ea3259b8977a862f242
Lars Albertsen <laal@stibosystems.com>
2024-11-01 02:01:58 -0500

    Committed from STEP 2024-11-01T07:01Z

---
+++ BusinessRule_DoNothing.js
@@ -0,0 +1,39 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Approved"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "DoNothing",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "LAALBRGroup" ],
+  "name" : "DoNothing",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ "Item" ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (node) {
+logger.info("DoNothing "+node);
+
+}
---
+++ BusinessRule_OnMainEvent.js
@@ -0,0 +1,43 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Approved"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "OnMainEvent",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "LAALBRGroup" ],
+  "name" : "OnMainEvent",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ "Item" ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (node) {
+logger.info("OnMainEvent "+node)
+
+node.getNonApprovedObjects().forEach(function(obj) {
+    logger.info("OnMainEvent: NonApprovedObject "+obj)
+    //obj.approve();
+})
+}

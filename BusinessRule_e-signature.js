e590d5428588fb2fbf902f0efd00926fc8352a83
Lars Albertsen <laal@stibosystems.com>
2024-11-12 01:00:17 +0800

    Committed from STEP 2024-11-11T17:00Z

--- BusinessRule_e-signature.js
+++ BusinessRule_e-signature.js
@@ -19,36 +19,3 @@
   "dependencies" : [ ]
 }
 */
-/*===== business rule plugin definition =====
-{
-  "pluginId" : "JavaScriptBusinessActionWithBinds",
-  "binds" : [ {
-    "contract" : "LoggerBindContract",
-    "alias" : "logger",
-    "parameterClass" : "null",
-    "value" : null,
-    "description" : null
-  }, {
-    "contract" : "BasicEsignatureBind",
-    "alias" : "basicESig",
-    "parameterClass" : "null",
-    "value" : null,
-    "description" : null
-  }, {
-    "contract" : "CurrentObjectBindContract",
-    "alias" : "node",
-    "parameterClass" : "null",
-    "value" : null,
-    "description" : null
-  } ],
-  "messages" : [ ],
-  "pluginType" : "Operation"
-}
-*/
-exports.operation0 = function (logger,basicESig,node) {
-logger.info("basicESig - before "+node.getTitle());
-logger.info("getTimestamt "+basicESig.getTimestamp());
-//logger.info("getReason "+basicESig.getReason());
-logger.info("basicESig - after");
-
-}

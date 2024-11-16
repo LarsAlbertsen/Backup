8d49ae8486480a49beba61191d256d340f1ef9c7
Lars Albertsen <laal@stibosystems.com>
2024-11-17 01:00:25 +0800

    Committed from STEP 2024-11-16T17:00Z

---
+++ BusinessRule_CreateCollection.js
@@ -0,0 +1,80 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Approved"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "CreateCollection",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "LAALRules" ],
+  "name" : "CreateCollection",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ "Product user-type root" ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "ManagerBindContract",
+    "alias" : "manager",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "ObjectTypeBindContract",
+    "alias" : "itemType",
+    "parameterClass" : "com.stibo.core.domain.impl.ObjectTypeImpl",
+    "value" : "Item",
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (manager,itemType) {
+var collection = manager.getNodeCollectionHome().getTopNodeCollectionGroup().createNodeCollection("myCold2");
+
+var rootProduct = manager.getProductHome().getTopProduct();
+
+var l = new java.util.HashSet()
+l.add(rootProduct)
+
+
+
+ var c = com.stibo.query.condition.Conditions;
+ var h = manager.getHome(com.stibo.query.home.QueryHome);
+ var querySpecification = h.queryFor(com.stibo.core.domain.Product).where(c.objectType(itemType));;
+ var query = querySpecification.execute();
+
+var count = 0;
+ 
+ query.forEach(function(node) {
+     //handle node here
+     count++;
+     if (count<1000) {
+     	if ((count%100)==0) {
+		    	logger.info("count "+count);
+     	}
+     	//var set = new java.util.HashSet();
+     	//set.add(node);
+     	//collection.addNodes(set);
+	     l.add(node);
+	     return true;
+     }
+     else {
+     	return false;
+     }
+ });
+
+ 
+collection.addNodes(l);
+
+}

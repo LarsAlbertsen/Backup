e776c187d43ff570713280cb2e112f0dddf1052b
Lars Albertsen <laal@stibosystems.com>
2025-01-07 01:00:23 +0800

    Committed from STEP 2025-01-06T17:00Z

---
+++ BusinessRule_DeleteUser.js
@@ -0,0 +1,50 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Approved"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "DeleteUser",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "LAALRules" ],
+  "name" : "DeleteUser",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : true,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "ManagerBindContract",
+    "alias" : "manager",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (manager) {
+
+var g = manager.getGroupHome().getGroupByID("GroupToDelete")
+g.delete()
+
+
+/*
+var u = manager.getUserHome().getUserByID("UserToDelete");
+u.delete();
+
+*/
+
+logger.info("Is he deleted?")
+
+}

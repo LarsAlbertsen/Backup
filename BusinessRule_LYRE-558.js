0e235f659df4c84d520a915291ed2a49fee4f833
Lars Albertsen <laal@stibosystems.com>
2025-01-30 01:00:23 +0800

    Committed from STEP 2025-01-29T17:00Z

--- BusinessRule_LYRE-558.js
+++ BusinessRule_LYRE-558.js
@@ -61,7 +61,7 @@
   }, {
     "contract" : "ProductBindContract",
     "alias" : "belowProduct",
-    "parameterClass" : "com.stibo.core.domain.impl.FrontProductImpl$$Generated$$12",
+    "parameterClass" : "com.stibo.core.domain.impl.FrontProductImpl$$Generated$$19",
     "value" : "102609",
     "description" : null
   } ],

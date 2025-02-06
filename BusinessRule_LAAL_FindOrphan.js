312d513785d8c03071d716fc7e75f2a091c7294e
Lars Albertsen <laal@stibosystems.com>
2025-02-06 02:01:54 -0500

    Committed from STEP 2025-02-06T07:01Z

---
+++ BusinessRule_LAAL_FindOrphan.js
@@ -0,0 +1,50 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Approved"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "LAAL_FindOrphan",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "LAALBRGroup" ],
+  "name" : "LAAL_FindOrphan",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ "Item" ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "item",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (item) {
+
+
+var allValues = item.getValues()
+
+allValues.forEach(function(value) {
+    if (value.isOrphan()) {
+        logger.info(item.getID()+ "  Value: " + value)  
+    }
+});
+
+
+
+
+}

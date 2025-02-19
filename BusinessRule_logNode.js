42655c2e5f1b2063a5676b1709a6a2cdb15b4e01
Lars Albertsen <laal@stibosystems.com>
2025-02-19 02:01:57 -0500

    Committed from STEP 2025-02-19T07:01Z

---
+++ BusinessRule_logNode.js
@@ -0,0 +1,44 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Approved"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "logNode",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "ELFRBRGroup" ],
+  "name" : "logNode",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : true,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "LoggerBindContract",
+    "alias" : "logger",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (node,logger) {
+logger.info('node ' + node)
+}

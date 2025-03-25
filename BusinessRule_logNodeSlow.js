7fc6f3cf651ca01e804ca5ac752147ba5cf0cd13
Lars Albertsen <laal@stibosystems.com>
2025-03-25 02:42:20 -0400

    Committed from STEP 2025-03-25T06:17Z

---
+++ BusinessRule_logNodeSlow.js
@@ -0,0 +1,45 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Approved"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "logNodeSlow",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "ELFRBRGroup" ],
+  "name" : "logNodeSlow",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : true,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "LoggerBindContract",
+    "alias" : "logger",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (node,logger) {
+java.lang.Thread.sleep(310*1000)
+logger.info('node ' + node)
+}

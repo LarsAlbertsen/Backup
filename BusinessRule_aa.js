c173008f44828cc71e7b740c3097e0d5973ba9c2
Lars Albertsen <laal@stibosystems.com>
2025-03-27 02:44:53 -0400

    Committed from STEP 2025-03-27T06:20Z

---
+++ BusinessRule_aa.js
@@ -0,0 +1,21 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Approved"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "aa",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "Actions" ],
+  "name" : "aa",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/

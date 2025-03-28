dbdf0202c39a26480bba068890c7477a99016cee
Lars Albertsen <laal@stibosystems.com>
2025-03-28 02:43:24 -0400

    Committed from STEP 2025-03-28T06:18Z

---
+++ BusinessRule_ELFRTrueWithLog.js
@@ -0,0 +1,65 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Approved"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "ELFRTrueWithLog",
+  "type" : "BusinessCondition",
+  "setupGroups" : [ "ELFRBRGroup" ],
+  "name" : "ELFRTrueWithLog",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : true,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessConditionWithBinds",
+  "binds" : [ {
+    "contract" : "LoggerBindContract",
+    "alias" : "logger",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "CurrentEventQueueBinding",
+    "alias" : "ceq",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "CurrentEventTypeBinding",
+    "alias" : "cet",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (logger,ceq,node,cet) {
+function log(msg) {
+	logger.info(msg)
+}
+
+log('node ' + node)
+log('Current Event Queue ' + ceq)
+log('Current Event Type ' + cet)
+
+
+return true
+}

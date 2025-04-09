ccbbc5aa9f0493b8dcb29a03c758f7529a60baeb
Lars Albertsen <laal@stibosystems.com>
2025-04-09 02:53:47 -0400

    Committed from STEP 2025-04-09T06:23Z

---
+++ BusinessRule_mnor_test.js
@@ -0,0 +1,53 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Approved"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "mnor_test",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "Actions" ],
+  "name" : "mnor_test",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ "Item" ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : true,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "ManagerBindContract",
+    "alias" : "manager",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "LoggerBindContract",
+    "alias" : "logger",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (node,manager,logger) {
+var prod = manager.getProductHome().getProductByID(node.getID());
+
+var parent = prod.getParent();
+var childArray = parent.getChildren().toArray();
+}

e858b808d37fbfbad87b52535bf8699169615995
Lars Albertsen <laal@stibosystems.com>
2025-06-12 01:00:21 -0400

    Committed from STEP 2025-06-12T05:00Z

---
+++ BusinessRule_AuditAction.js
@@ -0,0 +1,39 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Approved"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "AuditAction",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "LAALRules" ],
+  "name" : "AuditAction",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ "Item" ],
+  "allObjectTypesValid" : true,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentTransitionBindContract",
+    "alias" : "currentTransition",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (currentTransition) {
+logger.info('currentTransition '+currentTransition);
+
+}

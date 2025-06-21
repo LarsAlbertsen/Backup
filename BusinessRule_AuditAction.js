ab97f0be26d2291b4aeda113fdd6814b73205a9d
Lars Albertsen <laal@stibosystems.com>
2025-06-21 01:00:25 -0400

    Committed from STEP 2025-06-21T05:00Z

--- BusinessRule_AuditAction.js
+++ BusinessRule_AuditAction.js
@@ -28,14 +28,118 @@
     "parameterClass" : "null",
     "value" : null,
     "description" : null
+  }, {
+    "contract" : "ManagerBindContract",
+    "alias" : "manager",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "CurrentWorkflowBindContract",
+    "alias" : "workflow",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "TransitionEvaluationBindContract",
+    "alias" : "transitionEvaluation",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "AuditMessageTopicBindContract",
+    "alias" : "topic",
+    "parameterClass" : "com.stibo.auditmessaging.domain.impl.topic.AuditMessageTopicImpl",
+    "value" : "transitions",
+    "description" : null
   } ],
   "messages" : [ ],
   "pluginType" : "Operation"
 }
 */
-exports.operation0 = function (currentTransition) {
-logger.info('currentTransition from '+currentTransition.getSource().getID()+' to '+currentTransition.getTarget().getID());
+exports.operation0 = function (currentTransition,manager,node,workflow,transitionEvaluation,topic) {
+logger.info('currentTransition node='+node.getID()+' from '+currentTransition.getSource().getID()+' to '+currentTransition.getTarget().getID());
 
+
+// Workflow Audit Action version 1.0. June 2019, Component audit-messaging
+// NOTE: A bind must be made to the relevant audit messaging topic for this to produce an audit message, refer to the bottom of script
+var nodeID = node.getID();
+var userID = manager.getCurrentUser().getID();
+var workflowID = workflow.getID();
+
+var event = transitionEvaluation.getEvent();
+var eventID = null;
+if (event != null) {
+   eventID = event.getID();
+}
+
+var transitionMessage = transitionEvaluation.getMessage();
+var transitionRejected = transitionEvaluation.isRejected();
+var resultMessages = transitionEvaluation.getResultMessages();
+
+if (resultMessages.size() === 0) {
+   var concatenatedResults = null;
+} else {
+   var concatenatedResults = "Evaluation Results (" + resultMessages.size() + "): ";
+}
+
+var resultMessageIter = resultMessages.iterator();
+while (resultMessageIter.hasNext()) {
+   concatenatedResults = concatenatedResults + resultMessageIter.next() + "; ";
+}
+
+var sourceState = transitionEvaluation.getSource();
+var sourceStateID = null;
+if (sourceState) {
+   sourceStateID = sourceState.getID();
+}
+
+var targetState = transitionEvaluation.getTarget();
+var targetStateID = null;
+if (targetState) {
+   targetStateID = targetState.getID();
+}
+
+var logTime = new Date().getTime();
+
+// When using the Audit Message Receiver JDBC Delivery Plugin, the default behavior when processing 
+// messages is to "insert" each message into the database (i.e. create a new database entry for each 
+// message). If you wish to "upsert" messages (i.e. if a message with a matching ID exists, update 
+// the entry, otherwise create a new database entry), the audit message should contain a field with
+// the key "_ID". For example, the field could be set to be a combination of the nodeID and workflowID.
+//
+// var auditObject = {
+//    "_ID": "" + nodeID + "_" + workflowID,
+//    ...
+// }
+var auditObject = {
+   "nodeID": "" + nodeID,
+   "workflowID": "" + workflowID,
+   "userID": "" + userID,
+   "logTime": logTime,
+   "transition": {
+      "eventID": "" + eventID,
+      "submitMessage": "" + transitionMessage,
+      "sourceStateID": "" + sourceStateID,
+      "targetStateID": "" + targetStateID,
+      "isRejected": transitionRejected,
+      "rejectionMessages": "" + concatenatedResults
+   }
+};
+
+var auditMessage = JSON.stringify(auditObject);
+
+//--------------- UPDATE BELOW TO YOUR SYSTEM SETUP ---------------
+// Send audit message to the audit message framework. A bind will have to be made to a
+// topic of an audit message receiver plugin.
+topic.sendMessageAsync(auditMessage)
+node.approve();
 
 
 }
---
+++ BusinessRule_AuditApproval.js
@@ -0,0 +1,195 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Approved"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "AuditApproval",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "LAALRules" ],
+  "name" : "AuditApproval",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ "Item", "TestItem" ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Trigger",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "LoggerBindContract",
+    "alias" : "logger",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "ApproveContextBindContract",
+    "alias" : "ac",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "ManagerBindContract",
+    "alias" : "manager",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "AuditMessageTopicBindContract",
+    "alias" : "topic",
+    "parameterClass" : "com.stibo.auditmessaging.domain.impl.topic.AuditMessageTopicImpl",
+    "value" : "approvals",
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (node,logger,ac,manager,topic) {
+function nvl(v) {
+	return v?v:''
+}
+function getReference(n, refTypeid, tid) {
+	var foundRef = null
+	n.queryReferences(n.getManager().getReferenceTypeHome().getReferenceTypeByID(refTypeid)).forEach(function(ref) {
+		if (n.equals(ref.getSource()) && tid == ref.getTarget().getID()) {
+			foundRef = ref; 
+		}
+		return !foundRef
+	})
+	return foundRef
+}
+function hasRef(n, refTypeid, tid) {
+	return getReference(n, refTypeid, tid)? true : false
+}
+
+function getValue(n,aid) {
+	if (n) {
+		var v = n.getValue(aid);
+		if (v) {
+			if (v.getSimpleValue()) {
+				return v.getSimpleValue()
+			}
+		}	
+	}
+	return ''
+}
+
+function handleNamePartObject(nodeBefore, nodeAfter) {
+	var isNew = !nodeBefore
+	return {
+		type:'Name',
+		nameBefore: isNew?'':nvl(nodeBefore.getName()),
+		nameAfter : nvl(nodeAfter.getName())
+	}
+}
+function handleValuePartObject(po, nodeBefore, nodeAfter) {
+	return {
+		type:'Value',
+		aid:po.getAttributeID(),
+		valBefore : nvl(getValue(nodeBefore, po.getAttributeID())),
+		valAfter : nvl(getValue(nodeAfter, po.getAttributeID()))
+	}
+}
+function handleReferencePartObject(po, nodeBefore, nodeAfter) {
+	//NOTE, po is passed even if the target does not exist in Approved WS. 
+	//I.e., even the attempt to approve a reference, even though the target does not exist in Approved WS, results in a Part Object. 
+	//The approval will actually fail, so we may have to check if the target exists in Approved WS, and if not, ignore this po
+	//TODO: Ignore references to objects not found in Approved WS
+	var a = nodeAfter?hasRef(nodeAfter, po.getReferenceType(), po.getTargetID()):false;
+	var b = nodeBefore?hasRef(nodeBefore, po.getReferenceType(), po.getTargetID()):false;
+	var doc = {
+		type : po,
+		refType : po.getReferenceType(),
+		targetID : po.getTargetID(),
+		state : a&&b?'Update':(a&&!b?'New':'Delete'),
+		values : []
+	}
+	if (po instanceof com.stibo.core.domain.partobject.AssetReferencePartObject) {
+		doc.type = 'AssetReference'
+	} else if (po instanceof com.stibo.core.domain.partobject.ProductReferencePartObject) {
+		doc.type = 'ProductReference'
+	}
+	var refType = manager.getReferenceTypeHome().getReferenceTypeByID(po.getReferenceType());
+	var bRef = nodeBefore?getReference(nodeBefore, refType.getID(), po.getTargetID()):'';
+	var aRef = nodeAfter?getReference(nodeAfter, refType.getID(), po.getTargetID()):'';
+	refType.getValidDescriptionAttributes().toArray().forEach(function(attr) {
+		var bVal = bRef?getValue(bRef, attr.getID()):'';
+		var aVal = aRef?getValue(aRef, attr.getID()):'';
+		if (aVal != bVal) {
+			doc.values.push({
+				aid : attr.getID(),
+				valBefore : bVal,
+				valAfter : aVal
+			})
+		}
+	})
+	return doc;
+}
+function handleClassificationLinkPartObject(po, nodeBefore, nodeAfter) {
+	var doc = {
+		type : 'ClassificationLink',
+		linkType : po.getLinkTypeID(),
+		classID : po.getClassificationID()
+		//TODO, check if new, update or delete
+	}
+	return doc;
+}
+
+function getBase(n) {
+	return n instanceof com.stibo.core.domain.Product ? 'Product' :
+			n instanceof com.stibo.core.domain.Classification ? 'Classification' : 
+			n instanceof com.stibo.core.domain.Entity ? 'Entity' :
+			n instanceof com.stibo.core.domain.Asset ? 'Asset' : node
+}
+
+function getNodeState(ac) {
+	return ac.getApprovedNode() ? 'Update' : 'New'
+}
+
+//############################## MAIN ##############################
+//NOTE: Rule is not invoked when approving a deletion (i.e., approve in Recycle Bin
+var data = {
+	type: getBase(node),
+	id: node.getID(),
+	ctx: manager.getCurrentContext().getID(),
+	user: manager.getCurrentUser().getID(),
+	time: new Date(Date.now()).toISOString(),
+	state: getNodeState(ac),
+	changes: []
+}
+
+ac.getPartObjects().forEach(function (po) {
+	if (po instanceof com.stibo.core.domain.partobject.NamePartObject) {
+		data.changes.push(handleNamePartObject(ac.getApprovedNode(), ac.getMainNode()))
+	}
+	else if (po instanceof com.stibo.core.domain.partobject.ValuePartObject) {
+		data.changes.push(handleValuePartObject(po, ac.getApprovedNode(), ac.getMainNode()))
+	}
+	else if (po instanceof com.stibo.core.domain.partobject.ReferencePartObject) {
+		data.changes.push(handleReferencePartObject(po, ac.getApprovedNode(), ac.getMainNode()))
+	}
+	else if (po instanceof com.stibo.core.domain.partobject.ClassificationLinkPartObject) {
+		data.changes.push(handleClassificationLinkPartObject(po, ac.getApprovedNode(), ac.getMainNode()))
+	}
+     else {
+		logger.info('Unhandled ' + po)
+	}
+})
+
+logger.info(JSON.stringify(data))
+topic.sendMessageAsync(JSON.stringify(data))
+
+}

3bf3e3871bdf196f8ea911fadff06f83401404e2
Lars Albertsen <laal@stibosystems.com>
2025-06-26 01:00:25 -0400

    Committed from STEP 2025-06-26T05:00Z

---
+++ BusinessRule_CreatePublication.js
@@ -0,0 +1,39 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Approved"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "CreatePublication",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "LAALRules" ],
+  "name" : "Create Publication",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "ManagerBindContract",
+    "alias" : "manager",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (manager) {
+
+manager
+}

7a2448c2435ba5bccd0efaf8d08d752f381dffe3
Lars Albertsen <laal@stibosystems.com>
2025-07-31 01:32:25 -0400

    Committed from STEP 2025-07-31T05:05Z

---
+++ BusinessRule_ExtFunction.js
@@ -0,0 +1,32 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Approved"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "ExtFunction",
+  "type" : "BusinessFunction",
+  "setupGroups" : [ "TestBR" ],
+  "name" : "ExtFunction",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : true,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "ExternalBusinessFunction",
+  "parameters" : [ {
+    "id" : "ExternalBusinessFunction",
+    "type" : "com.stibo.integration.extension.domain.parameter.ExternalBusinessFunctionExtensionConfiguration",
+    "value" : "eyJyZXR1cm5UeXBlIjoiU3RyaW5nIiwiZW5naW5lSWQiOiJjb252ZXJ0LXRvLWV4Y2VsIiwiaWQiOiJDb252ZXJ0VG9FeGNlbCIsInVybCI6Imh0dHA6Ly9leHRlbnNpb24tZW5naW5lLWNvbnZlcnQtdG8tZXhjZWwucHJvZHVjdHBsYXRmb3JtLWxhYWwuc3ZjLmNsdXN0ZXIubG9jYWw6ODA4MC9leHRlbnNpb25zL0NvbnZlcnRUb0V4Y2VsIiwicGFyYW1ldGVycyI6eyJib29sZWFuUGFyYW1ldGVyMiI6IkJvb2xlYW4iLCJzdHJpbmdQYXJhbWV0ZXIyIjoiU3RyaW5nIn0sInRpbWVvdXQiOjEwMDAwLCJmYWlsdXJlU3RyYXRlZ3kiOiJGQUlMIiwic2lnbmF0dXJlIjoiMzkwNWQ5YzYifQ=="
+  } ],
+  "pluginType" : "Operation"
+}
+*/
---
+++ BusinessRule_orphanValues.js
@@ -0,0 +1,90 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Approved"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "orphanValues",
+  "type" : "BusinessFunction",
+  "setupGroups" : [ "ELFRBRGroup" ],
+  "name" : "orphanValues",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : true,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessFunctionWithBinds",
+  "binds" : [ {
+    "contract" : "LoggerBindContract",
+    "alias" : "logger",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation",
+  "functionReturnType" : "java.lang.String",
+  "functionParameterBinds" : [ {
+    "contract" : "NodeBindContract",
+    "alias" : "root",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : "Look here and below"
+  } ]
+}
+*/
+exports.operation0 = function (logger,root) {
+function log(msg) {
+	logger.info(msg)
+}
+
+logger.info("Lars A...")
+
+var oCount = 0;
+var nodeCount = 0;
+var seen = {}
+function checkNode(n) {
+	var orphans = {}
+	nodeCount++
+	n.getValues().toArray().forEach(v => {
+		if (v.isLocal()) {
+			if (v.isOrphan()) {
+				var a = v.getAttribute().getID() + ''
+				if (!seen[a]) {
+					seen[a] = []
+				}
+				seen[a].push(n.getID()+'');
+				oCount++
+				orphans[a] = v.getSimpleValue()
+			}
+		}
+	})
+	
+	//n.queryChildren().forEach(child => {
+	//	checkNode(child)
+	//	return true;	
+	//})
+	return orphans
+}
+
+
+
+//############################## MAIN ##############################
+var res = checkNode(root)
+log('Node Count ' + nodeCount)
+log('Orphan Count ' + oCount)
+//log(JSON.stringify(seen))
+//Object.keys(seen).forEach(a=> {
+//	log(a + '\t' + seen[a].length + '\t\t' + seen[a].slice(0,5) + '......')
+//})
+return JSON.stringify(res)
+
+}

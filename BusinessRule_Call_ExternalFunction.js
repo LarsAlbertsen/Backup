1916e1d20e62e8cf920f87e9d7e2fa89578e6e0e
Lars Albertsen <laal@stibosystems.com>
2025-08-07 01:32:43 -0400

    Committed from STEP 2025-08-07T05:05Z

--- BusinessRule_Call_ExternalFunction.js
+++ BusinessRule_Call_ExternalFunction.js
@@ -26,7 +26,7 @@
     "contract" : "BusinessFunctionBindContract",
     "alias" : "externalFunction",
     "parameterClass" : "com.stibo.core.domain.impl.businessrule.function.javascript.reference.BusinessFunctionReferenceImpl",
-    "value" : "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<BusinessFunctionReference>\n  <BusinessFunction>ExternalFunction</BusinessFunction>\n</BusinessFunctionReference>\n",
+    "value" : "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<BusinessFunctionReference>\n  <BusinessFunction>ConvertToExcel</BusinessFunction>\n</BusinessFunctionReference>\n",
     "description" : null
   }, {
     "contract" : "TempStoreBindContract",
@@ -41,12 +41,23 @@
 */
 exports.operation0 = function (externalFunction,tmpStore) {
 
+const value = 
+`header1|header2|header3
+a1|a2|a3
+b1|b2|b3
+c1|c2|c3
+d1|d2|d3
+e1|e2|e3`
+;
+
 const myFile = tmpStore.create("file.tmp");
+myFile.write(value);
+myFile.close();
 
 let args = new java.util.HashMap();
-args.put("booleanParameter1", true);
-args.put("stringParameter1", "Lars");
-//args.put("payload", myFile);
+args.put("booleanParameter2", true);
+args.put("stringParameter2", "Lars");
+args.put("payload", myFile);
 const result = externalFunction.evaluate(args);
 
 logger.info("External function executed with arguments: " + JSON.stringify(result));
--- BusinessRule_ConvertToExcel.js
+++ BusinessRule_ConvertToExcel.js
@@ -19,3 +19,14 @@
   "dependencies" : [ ]
 }
 */
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "ExternalBusinessFunction",
+  "parameters" : [ {
+    "id" : "ExternalBusinessFunction",
+    "type" : "com.stibo.integration.extension.domain.parameter.ExternalBusinessFunctionExtensionConfiguration",
+    "value" : "eyJyZXR1cm5UeXBlIjoiU3RyaW5nIiwiZW5naW5lSWQiOiJjb252ZXJ0LXRvLWV4Y2VsIiwiaWQiOiJDb252ZXJ0VG9FeGNlbCIsInVybCI6Imh0dHA6Ly9leHRlbnNpb24tZW5naW5lLWNvbnZlcnQtdG8tZXhjZWwucHJvZHVjdHBsYXRmb3JtLWxhYWwuc3ZjLmNsdXN0ZXIubG9jYWw6ODA4MC9leHRlbnNpb25zL0NvbnZlcnRUb0V4Y2VsIiwicGFyYW1ldGVycyI6eyJwYXlsb2FkIjoiUGF5bG9hZCIsImJvb2xlYW5QYXJhbWV0ZXIyIjoiQm9vbGVhbiIsInN0cmluZ1BhcmFtZXRlcjIiOiJTdHJpbmcifSwidGltZW91dCI6MTAwMDAsImZhaWx1cmVTdHJhdGVneSI6IkZBSUwiLCJzaWduYXR1cmUiOiI5MGM0YWMzNSJ9"
+  } ],
+  "pluginType" : "Operation"
+}
+*/

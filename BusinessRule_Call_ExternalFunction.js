ffa1c3af90ae42fff43d31ea631344651ca5375d
Lars Albertsen <laal@stibosystems.com>
2025-08-08 01:33:03 -0400

    Committed from STEP 2025-08-08T05:05Z

--- BusinessRule_Call_ExternalFunction.js
+++ BusinessRule_Call_ExternalFunction.js
@@ -34,13 +34,25 @@
     "parameterClass" : "null",
     "value" : null,
     "description" : null
+  }, {
+    "contract" : "BusinessFunctionBindContract",
+    "alias" : "convertFromExcel",
+    "parameterClass" : "com.stibo.core.domain.impl.businessrule.function.javascript.reference.BusinessFunctionReferenceImpl",
+    "value" : "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<BusinessFunctionReference>\n  <BusinessFunction>ConvertFromExcel2</BusinessFunction>\n</BusinessFunctionReference>\n",
+    "description" : null
+  }, {
+    "contract" : "AssetBindContract",
+    "alias" : "excelAsset",
+    "parameterClass" : "com.stibo.core.domain.impl.FrontAssetImpl$$Generated$$25",
+    "value" : "32158417",
+    "description" : null
   } ],
   "messages" : [ ],
   "pluginType" : "Operation"
 }
 */
-exports.operation0 = function (externalFunction,tmpStore) {
-
+exports.operation0 = function (externalFunction,tmpStore,convertFromExcel,excelAsset) {
+/*
 const value = 
 `header1|header2|header3
 a1|a2|a3
@@ -61,4 +73,26 @@ args.put("payload", myFile);
 const result = externalFunction.evaluate(args);
 
 logger.info("External function executed with arguments: " + JSON.stringify(result));
+*/
+
+const myFile = tmpStore.create("file.tmp");
+excelAsset.download(myFile.outputStream())
+myFile.close();
+
+let args = new java.util.HashMap();
+args.put("excelFile", myFile);
+
+let str = convertFromExcel.evaluate(args);
+
+
+logger.info("Result: " + str);
+
+
+/** @type{Payload) *
+const jsonPayload = convertFromExcel.evaluate(args);
+var bytes = jsonPayload.inputStream().readAllBytes();
+logger.info("Read " + bytes.length + " bytes from JSON payload");
+str = new java.lang.String(bytes, java.nio.charset.StandardCharsets.UTF_8);
+logger.info("Converted JSON: " + str);
+*/
 }
---
+++ BusinessRule_ConvertFromExcel.js
@@ -0,0 +1,32 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Approved"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "ConvertFromExcel",
+  "type" : "BusinessFunction",
+  "setupGroups" : [ "Extensions" ],
+  "name" : "ConvertFromExcel",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : true,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "ExternalBusinessFunction",
+  "parameters" : [ {
+    "id" : "ExternalBusinessFunction",
+    "type" : "com.stibo.integration.extension.domain.parameter.ExternalBusinessFunctionExtensionConfiguration",
+    "value" : "eyJyZXR1cm5UeXBlIjoiUGF5bG9hZCIsImVuZ2luZUlkIjoiY29udmVydC10by1leGNlbCIsImlkIjoiQ29udmVydEZyb21FeGNlbCIsInVybCI6Imh0dHA6Ly9leHRlbnNpb24tZW5naW5lLWNvbnZlcnQtdG8tZXhjZWwucHJvZHVjdHBsYXRmb3JtLWxhYWwuc3ZjLmNsdXN0ZXIubG9jYWw6ODA4MC9leHRlbnNpb25zL0NvbnZlcnRGcm9tRXhjZWwiLCJwYXJhbWV0ZXJzIjp7ImV4Y2VsRmlsZSI6IlBheWxvYWQifSwidGltZW91dCI6MTAwMDAsImZhaWx1cmVTdHJhdGVneSI6IkZBSUwiLCJzaWduYXR1cmUiOiJiODVkYjQ3ZCJ9"
+  } ],
+  "pluginType" : "Operation"
+}
+*/

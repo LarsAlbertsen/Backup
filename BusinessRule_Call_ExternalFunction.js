64f503ad2c5162ce4c40901b1595d61ed65ed4d0
Lars Albertsen <laal@stibosystems.com>
2025-08-29 01:28:00 -0400

    Committed from STEP 2025-08-29T05:05Z

--- BusinessRule_Call_ExternalFunction.js
+++ BusinessRule_Call_ExternalFunction.js
@@ -37,7 +37,7 @@
   }, {
     "contract" : "AssetBindContract",
     "alias" : "excelAsset",
-    "parameterClass" : "com.stibo.core.domain.impl.FrontAssetImpl$$Generated$$25",
+    "parameterClass" : "com.stibo.core.domain.impl.FrontAssetImpl$$Generated$$36",
     "value" : "32158417",
     "description" : null
   }, {

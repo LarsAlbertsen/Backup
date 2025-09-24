8b4d7753a7061bf2903757273f2f045463f55ea7
Lars Albertsen <laal@stibosystems.com>
2025-09-24 01:36:06 -0400

    Committed from STEP 2025-09-24T05:06Z

---
+++ BusinessRule_AssetImportConfiguration.js
@@ -0,0 +1,47 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Approved"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "AssetImportConfiguration",
+  "type" : "BusinessCondition",
+  "setupGroups" : [ "ELFRBRGroup" ],
+  "name" : "AssetImportConfiguration",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : true,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessConditionWithBinds",
+  "binds" : [ {
+    "contract" : "LoggerBindContract",
+    "alias" : "logger",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (logger,node) {
+logger.info(node)
+logger.info(node.getValue('asset.filename').getSimpleValue())
+
+return 'no no no';
+}

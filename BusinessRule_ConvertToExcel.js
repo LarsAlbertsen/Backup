4bd21c668780bed291f2840098215b7ebd994007
Lars Albertsen <laal@stibosystems.com>
2025-10-01 01:34:17 -0400

    Committed from STEP 2025-10-01T05:06Z

--- BusinessRule_ConvertToExcel.js
+++ BusinessRule_ConvertToExcel.js
@@ -25,7 +25,7 @@
   "parameters" : [ {
     "id" : "ExternalBusinessFunction",
     "type" : "com.stibo.integration.extension.domain.parameter.ExternalBusinessFunctionExtensionConfiguration",
-    "value" : "eyJyZXR1cm5UeXBlIjoiUGF5bG9hZCIsImVuZ2luZUlkIjoibGFhbC0zIiwiaWQiOiJDb252ZXJ0VG9FeGNlbCIsInVybCI6Imh0dHA6Ly9leHRlbnNpb24tZW5naW5lLWxhYWwtMy5wcm9kdWN0cGxhdGZvcm0tbGFhbC5zdmMuY2x1c3Rlci5sb2NhbDo4MDgwL2FwaS9leHRlbnNpb25zL0NvbnZlcnRUb0V4Y2VsIiwicGFyYW1ldGVycyI6eyJjc3ZGaWxlIjoiUGF5bG9hZCJ9LCJ0aW1lb3V0IjoxMDAwMCwiZmFpbHVyZVN0cmF0ZWd5IjoiRkFJTCIsInNpZ25hdHVyZSI6IjE5OTk2NjFiIn0="
+    "value" : "eyJyZXR1cm5UeXBlIjoiUGF5bG9hZCIsImVuZ2luZUlkIjoiY29udmVydC1leGNlbCIsImlkIjoiQ29udmVydFRvRXhjZWwiLCJ1cmwiOiJodHRwOi8vZXh0ZW5zaW9uLWVuZ2luZS1jb252ZXJ0LWV4Y2VsOjgwODAvYXBpL2V4dGVuc2lvbnMvQ29udmVydFRvRXhjZWwiLCJwYXJhbWV0ZXJzIjp7ImNzdkZpbGUiOiJQYXlsb2FkIn0sInRpbWVvdXQiOjEwMDAwLCJmYWlsdXJlU3RyYXRlZ3kiOiJGQUlMIiwic2lnbmF0dXJlIjoiMTk5OTY2MWIifQ=="
   } ],
   "pluginType" : "Operation"
 }

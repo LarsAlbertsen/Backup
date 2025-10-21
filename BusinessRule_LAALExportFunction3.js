7efb8a7e38a23daffcc8ae44e6569a940b3a270c
Lars Albertsen <laal@stibosystems.com>
2025-10-21 01:33:46 -0400

    Committed from STEP 2025-10-21T05:06Z

--- BusinessRule_LAALExportFunction3.js
+++ BusinessRule_LAALExportFunction3.js
@@ -6,10 +6,10 @@
 */
 /*===== business rule definition =====
 {
-  "id" : "LAALExportFunction",
+  "id" : "LAALExportFunction3",
   "type" : "BusinessFunction",
   "setupGroups" : [ "LAALExport" ],
-  "name" : "LAALExportFunction",
+  "name" : "LAALExportFunction3",
   "description" : null,
   "scope" : "Global",
   "validObjectTypes" : [ ],
@@ -25,7 +25,7 @@
   "parameters" : [ {
     "id" : "ExternalBusinessFunction",
     "type" : "com.stibo.integration.extension.domain.parameter.ExternalBusinessFunctionExtensionConfiguration",
-    "value" : "eyJyZXR1cm5UeXBlIjoiUGF5bG9hZCIsImVuZ2luZUlkIjoiY29udmVydC1leGNlbCIsImlkIjoiTEFBTEV4cG9ydCIsInVybCI6Imh0dHA6Ly9leHRlbnNpb24tZW5naW5lLWNvbnZlcnQtZXhjZWw6ODA4MC9hcGkvZXh0ZW5zaW9ucy9MQUFMRXhwb3J0IiwicGFyYW1ldGVycyI6eyJpbnRlZ2VyUGFyYW1ldGVySWQiOiJJbnRlZ2VyIiwiZGF0YUZpbGUiOiJQYXlsb2FkIn0sInRpbWVvdXQiOjEyMDAwMCwiZmFpbHVyZVN0cmF0ZWd5IjoiRkFJTCIsInNpZ25hdHVyZSI6IjQzY2YwODZhIn0="
+    "value" : "eyJyZXR1cm5UeXBlIjoiUGF5bG9hZCIsImVuZ2luZUlkIjoiY29udmVydC1leGNlbCIsImlkIjoiTEFBTEV4cG9ydCIsInVybCI6Imh0dHA6Ly9leHRlbnNpb24tZW5naW5lLWNvbnZlcnQtZXhjZWw6ODA4MC9hcGkvZXh0ZW5zaW9ucy9MQUFMRXhwb3J0IiwicGFyYW1ldGVycyI6eyJ0aHJlYWRzIjoiSW50ZWdlciIsImRhdGFGaWxlIjoiUGF5bG9hZCJ9LCJ0aW1lb3V0Ijo2MDAwMDAsImZhaWx1cmVTdHJhdGVneSI6IkZBSUwiLCJzaWduYXR1cmUiOiI3YjFiMjY5NSJ9"
   } ],
   "pluginType" : "Operation"
 }
--- BusinessRule_LAALTestExport.js
+++ BusinessRule_LAALTestExport.js
@@ -26,7 +26,7 @@
     "contract" : "BusinessFunctionBindContract",
     "alias" : "LAALExportFunction",
     "parameterClass" : "com.stibo.core.domain.impl.businessrule.function.javascript.reference.BusinessFunctionReferenceImpl",
-    "value" : "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<BusinessFunctionReference>\n  <BusinessFunction>LAALExportFunction</BusinessFunction>\n</BusinessFunctionReference>\n",
+    "value" : "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<BusinessFunctionReference>\n  <BusinessFunction>LAALExportFunction3</BusinessFunction>\n</BusinessFunctionReference>\n",
     "description" : null
   }, {
     "contract" : "TempStoreBindContract",
@@ -54,7 +54,8 @@
 exports.operation0 = function (LAALExportFunction,tmpStore,manager,itemType) {
 
 
-
+const numberOfProducts = 10000;
+const maxThreads = 4
 
 
 var c = com.stibo.query.condition.Conditions;
@@ -62,30 +63,39 @@ var h = manager.getHome(com.stibo.query.home.QueryHome);
 var querySpecification = h.queryFor(com.stibo.core.domain.Product).where(c.objectType(itemType));;
 var query = querySpecification.execute();
 
+
 var count = 0;
-var total = 0;
 var l = new java.lang.StringBuilder()
 
 query.forEach(function (node) {
-  count++;
-  if (count <= 500000) {
-    l.append(node.getID()+"|");
+  if (++count <= numberOfProducts) {
+    l.append(node.getID() + "|");
     return true;
   }
   else {
     return false;
   }
 });
-logger.info("Arg length "+l.length());
+logger.info("ProductCount="+numberOfProducts+" ArgSize=" + l.length());
 
 const productIDs = tmpStore.create("args");
 productIDs.write(l.toString());
 productIDs.close();
 
+for (var i = 1; i <= maxThreads; i++) {
+  var startTime = new Date().getTime();
+  var args = new java.util.HashMap()
+  args.put("dataFile", productIDs);
+  args.put("threads", new java.lang.Integer(i));
+  //logger.info("args=" + args);
+  LAALExportFunction.evaluate(args);
 
-var args = new java.util.HashMap()
-args.put("dataFile", productIDs);
-LAALExportFunction.evaluate(args);
+
+  var endTime = new Date().getTime();
+  var duration = (endTime - startTime) / 1000;
+  logger.info("Number of Threads=" + i + " Duration=" + duration.toFixed(1) + " seconds");
+}
+
 
 
 }

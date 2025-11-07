574336b463b588a898a5526bc34676d9a355b75e
Lars Albertsen <laal@stibosystems.com>
2025-11-07 01:38:23 -0500

    Committed from STEP 2025-11-07T06:03Z

--- BusinessRule_LAALTestExport.js
+++ BusinessRule_LAALTestExport.js
@@ -55,7 +55,7 @@ exports.operation0 = function (LAALExportFunction,tmpStore,manager,itemType) {
 
 
 const numberOfProducts = 10000;
-const maxThreads = 8
+const maxThreads = 5
 
 
 var c = com.stibo.query.condition.Conditions;
---
+++ BusinessRule_ResetName.js
@@ -0,0 +1,39 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Approved"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "ResetName",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "LAALRules" ],
+  "name" : "ResetName",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ "Item" ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (node) {
+node.setName("Hello World");
+
+}

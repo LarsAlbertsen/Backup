9aa864916a1305d0dc92535de6bb3fdf4dd7e8ba
Lars Albertsen <laal@stibosystems.com>
2025-11-12 01:44:55 -0500

    Committed from STEP 2025-11-12T06:04Z

---
+++ BusinessRule_MyFunc.js
@@ -0,0 +1,32 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Approved"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "MyFunc",
+  "type" : "BusinessFunction",
+  "setupGroups" : [ "Extensions" ],
+  "name" : "MyFunc",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : true,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "ExternalBusinessFunction",
+  "parameters" : [ {
+    "id" : "ExternalBusinessFunction",
+    "type" : "com.stibo.integration.extension.domain.parameter.ExternalBusinessFunctionExtensionConfiguration",
+    "value" : "eyJyZXR1cm5UeXBlIjoiUGF5bG9hZCIsImVuZ2luZUlkIjoibXlleHR0IiwiaWQiOiJDb252ZXJ0RnJvbUV4Y2VsIiwidXJsIjoiaHR0cDovL2V4dGVuc2lvbi1lbmdpbmUtbXlleHR0OjgwODAvYXBpL2V4dGVuc2lvbnMvQ29udmVydEZyb21FeGNlbCIsInBhcmFtZXRlcnMiOnsiZXhjZWxGaWxlIjoiUGF5bG9hZCJ9LCJ0aW1lb3V0IjoxMDAwMCwiZmFpbHVyZVN0cmF0ZWd5IjoiRkFJTCIsInNpZ25hdHVyZSI6ImI4NWRiNDdkIn0="
+  } ],
+  "pluginType" : "Operation"
+}
+*/

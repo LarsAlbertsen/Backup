a7a532782a0850a09a180757198eeba7e767b0e9
Lars Albertsen <laal@stibosystems.com>
2025-12-13 03:48:14 -0500

    Committed from STEP 2025-12-13T08:14Z

---
+++ BusinessRule_AddNumbers.js
@@ -0,0 +1,32 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Approved"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "AddNumbers",
+  "type" : "BusinessFunction",
+  "setupGroups" : [ "Extensions" ],
+  "name" : "AddNumbers",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : true,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "ExternalBusinessFunction",
+  "parameters" : [ {
+    "id" : "ExternalBusinessFunction",
+    "type" : "com.stibo.integration.extension.domain.parameter.ExternalBusinessFunctionExtensionConfiguration",
+    "value" : "eyJyZXR1cm5UeXBlIjoiSW50ZWdlciIsImVuZ2luZUlkIjoibGFhbC1leHRlbnNpb24iLCJpZCI6IkFkZE51bWJlcnMiLCJ1cmwiOiJodHRwOi8vZXh0ZW5zaW9uLWVuZ2luZS1sYWFsLWV4dGVuc2lvbjo4MDgwL2FwaS9leHRlbnNpb25zL0FkZE51bWJlcnMiLCJwYXJhbWV0ZXJzIjp7ImZpcnN0IjoiSW50ZWdlciIsInNlY29uZCI6IkludGVnZXIifSwidGltZW91dCI6MTAwMDAsImZhaWx1cmVTdHJhdGVneSI6IkZBSUwiLCJzaWduYXR1cmUiOiJjZDU3YmU5OCJ9"
+  } ],
+  "pluginType" : "Operation"
+}
+*/
---
+++ BusinessRule_AddNumbersAction.js
@@ -0,0 +1,54 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Approved"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "AddNumbersAction",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "Extensions" ],
+  "name" : "AddNumbersAction",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ ],
+  "allObjectTypesValid" : true,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "BusinessFunctionBindContract",
+    "alias" : "add",
+    "parameterClass" : "com.stibo.core.domain.impl.businessrule.function.javascript.reference.BusinessFunctionReferenceImpl",
+    "value" : "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<BusinessFunctionReference>\n  <BusinessFunction>AddNumbers</BusinessFunction>\n</BusinessFunctionReference>\n",
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (add) {
+const numberOfCalls = 10000;
+
+const param = new java.util.HashMap();
+param.put("first", new java.lang.Integer(1))
+param.put("second", new java.lang.Integer(2))
+
+const begin = Date.now();
+let totalSum = 0
+for (let i = 0; i < numberOfCalls; i++) {
+    const sum = add.evaluate(param);
+    totalSum += sum;
+}
+const end = Date.now();
+const duration = end - begin;
+logger.info("Duration for "+numberOfCalls+" calls: "+duration+" ms");
+logger.info("Average duration per call: "+(duration/numberOfCalls)+" ms");
+
+}

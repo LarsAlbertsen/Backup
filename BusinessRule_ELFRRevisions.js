b3e04256aa1bb54f2fe39890dfeb5e0be903b698
Lars Albertsen <laal@stibosystems.com>
2026-01-02 13:12:19 -0500

    Committed from STEP 2026-01-02T17:37Z

---
+++ BusinessRule_ELFRRevisions.js
@@ -0,0 +1,54 @@
+/*===== export metadata =====
+{
+  "contextId" : "Context1",
+  "workspaceId" : "Approved"
+}
+*/
+/*===== business rule definition =====
+{
+  "id" : "ELFRRevisions",
+  "type" : "BusinessAction",
+  "setupGroups" : [ "ELFRBRGroup" ],
+  "name" : "ELFRRevisions",
+  "description" : null,
+  "scope" : "Global",
+  "validObjectTypes" : [ "Item" ],
+  "allObjectTypesValid" : false,
+  "runPrivileged" : false,
+  "onApprove" : "Never",
+  "dependencies" : [ ]
+}
+*/
+/*===== business rule plugin definition =====
+{
+  "pluginId" : "JavaScriptBusinessActionWithBinds",
+  "binds" : [ {
+    "contract" : "CurrentObjectBindContract",
+    "alias" : "node",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  }, {
+    "contract" : "LoggerBindContract",
+    "alias" : "logger",
+    "parameterClass" : "null",
+    "value" : null,
+    "description" : null
+  } ],
+  "messages" : [ ],
+  "pluginType" : "Operation"
+}
+*/
+exports.operation0 = function (node,logger) {
+var revs = []
+node.getRevisions().toArray().every(function(rev) {
+	revs.push(rev)
+	return true
+})
+//Putting oldest first, and newest last
+revs = revs.reverse()
+
+
+logger.info(revs.length)
+
+}

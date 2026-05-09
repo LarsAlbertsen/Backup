f27296d9e8295627b455f98771a0757da6b33fd4
Lars Albertsen <laal@stibosystems.com>
2026-05-09 01:02:54 -0400

    Committed from STEP 2026-05-09T05:02Z

--- BusinessRule_ELFRLog.js
+++ BusinessRule_ELFRLog.js
@@ -40,17 +40,25 @@
     "parameterClass" : "com.stibo.core.domain.impl.businessrule.function.javascript.reference.BusinessFunctionReferenceImpl",
     "value" : "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<BusinessFunctionReference>\n  <BusinessFunction>hasOrphanValues</BusinessFunction>\n</BusinessFunctionReference>\n",
     "description" : null
+  }, {
+    "contract" : "SecretBindContract",
+    "alias" : "secret",
+    "parameterClass" : "com.stibo.passwordparameter.PasswordParameter",
+    "value" : "eRAjNzxQJ81jAUVjF4v9QKYh6xLvP9orVbX4DWDI+7s=",
+    "description" : null
   } ],
   "messages" : [ ],
   "pluginType" : "Operation"
 }
 */
-exports.operation0 = function (node,logger,fn) {
+exports.operation0 = function (node,logger,fn,secret) {
 function log(msg) {
 	logger.info('ELFRLog : ' + msg)
 }
 log(node)
 
 fn.evaluate({root:node})
+
+logger.info(secret)
 
 }

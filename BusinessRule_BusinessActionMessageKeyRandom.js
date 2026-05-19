4dda871c872062c46f9c7e720efa159b315302ca
Lars Albertsen <laal@stibosystems.com>
2026-05-19 01:02:43 -0400

    Committed from STEP 2026-05-19T05:02Z

--- BusinessRule_BusinessActionMessageKeyRandom.js
+++ BusinessRule_BusinessActionMessageKeyRandom.js
@@ -6,10 +6,10 @@
 */
 /*===== business rule definition =====
 {
-  "id" : "BusinessActionMessageKey",
+  "id" : "BusinessActionMessageKeyRandom",
   "type" : "BusinessFunction",
   "setupGroups" : [ "BusinessActionOIEP" ],
-  "name" : "BusinessActionMessageKey",
+  "name" : "BusinessActionMessageKeyRandom",
   "description" : null,
   "scope" : "Global",
   "validObjectTypes" : [ ],
@@ -42,11 +42,25 @@
 }
 */
 exports.operation0 = function (logger,msg) {
+function generateUUID() { // Public Domain/MIT
+    var d = new Date().getTime();//Timestamp
+    var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16;//random number between 0 and 16
+        if(d > 0){//Use timestamp until depleted
+            r = (d + r)%16 | 0;
+            d = Math.floor(d/16);
+        } else {//Use microseconds since page-load if supported
+            r = (d2 + r)%16 | 0;
+            d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
 
 var doc = JSON.parse(msg)
 var map = new java.util.HashMap()
-map.put('MessageKey', doc[0].index?doc[0].index:doc[0].id)
-map.put('Name', doc[0].message?doc[0].message:doc[0].name)
+map.put('MessageKey', generateUUID())
 return map
 
 
